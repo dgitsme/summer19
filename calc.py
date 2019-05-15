957829209bf9fd42211839152474b98ac87af516
* <dgitsme>
2019-05-15 19:46:38 +0530

    Calc

--- calc.py
+++ calc.py
@@ -1,26 +1,23 @@
-#!usr/bin/python3
+#!usr/bin/env python3
 
 print("WELCOME TO BASIC CALCULATOR OF TWO NUMBERS")
 print("Enter the Two numbers for calculation")
-input(a)
-input(b)
+a=int(input())
+b=int(input())
 
 print("Choose one of the following operations to perform:")
 
 print("1.ADD/2.SUBTRACT/3.MULTIPLY/4.DIVIDE")
-input(c)
-def choice(c)
-switcher={ 
-1:
-Ans=a+b
-2:
-Ans=a-b
-3:
-Ans=a*b
-4:
-Ans=a/b
-}
-return switcher.get (c,"wrong choice")
-print('Your answer is '+ Ans)
+c=int(input())
+
+def choice(c):
+   switcher ={ 
+      1: a+b,
+      2: a-b,
+      3: a*b,
+      4: a/b,
+   }
+   return switcher.get(c,"wrong choice")
+print("Your answer is" + str(choice(c)))
 
 
